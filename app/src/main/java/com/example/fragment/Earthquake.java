cf679a04ee0654954b7c2d6200d97fdbb6d28a23
Ayan De <deayan252@gmail.com>
2023-06-22 19:45:30 +0530

    "Earthquake Constructor class added"

---
+++ app/src/main/java/com/example/fragment/Earthquake.java
@@ -0,0 +1,27 @@
+package com.example.fragment;
+
+public class Earthquake {
+
+    private String mMagnitude;
+    private String mLocation;
+    private String mDate;
+
+    public Earthquake(String magnitude, String location, String date)
+    {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mDate = date;
+    }
+
+    public String getmMagnitude(){
+        return mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+}
